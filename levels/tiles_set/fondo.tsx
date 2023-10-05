db08e03c239a2eaaf457aae424f062133dcad689
Abigail <129550135+abigailgonzalez2@users.noreply.github.com>
2023-10-05 13:13:27 -0300

    Add files via upload

---
+++ levels/tiles_set/fondo.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="fondo" tilewidth="3840" tileheight="640" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="886" height="384" source="../../../Users/abiga/Downloads/2 - Level/2 - Level/graphics/decoration/fondo/fondoo.png"/>
+ </tile>
+ <tile id="1">
+  <image width="3840" height="384" source="../../../Users/abiga/Downloads/2 - Level/2 - Level/graphics/decoration/fondo/fondo prueba.png"/>
+ </tile>
+ <tile id="2">
+  <image width="1408" height="640" source="../../../Users/abiga/Downloads/2 - Level/2 - Level/graphics/decoration/fondo/fondo_f.png"/>
+ </tile>
+ <tile id="3">
+  <image width="1024" height="640" source="../../../Users/abiga/Downloads/2 - Level/2 - Level/graphics/decoration/fondo/fondo_ff.png"/>
+ </tile>
+</tileset>
---
+++ levels/tiles_set/houses.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="houses" tilewidth="256" tileheight="331" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="190" height="256" source="../../../Users/abiga/Downloads/2 - Level/2 - Level/graphics/terrain/houses/house_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="192" height="215" source="../../../Users/abiga/Downloads/2 - Level/2 - Level/graphics/terrain/houses/house_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="331" source="../../../Users/abiga/Downloads/2 - Level/2 - Level/graphics/terrain/houses/house_3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="192" height="213" source="../../../Users/abiga/Downloads/2 - Level/2 - Level/graphics/terrain/houses/house_4.png"/>
+ </tile>
+</tileset>
